5e64544aa6b67dc879c9ed0e3bf4cd8b7fbd486d
Unknown <einar_weltan@hotmail.com>
2018-01-18 15:10:52 +0100

    Created Main Menu Directory

---
+++ public_html/mainMenu/script.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("gameScreen").innerHTML =
+            this.responseText;
+       }
+    };
+    xhttp.open("GET", "stages/stage01/stage01.html", true);
+    xhttp.send(); 
+}
