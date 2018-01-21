f0fb9d3124690b547702dc1bc67b1e6651e9d2f8
Unknown <olav.telseth94@hotmail.com>
2018-01-21 18:31:18 +0100

    Finished letter falling

--- public_html/startScreen/startScreen.js
+++ public_html/startScreen/startScreen.js
@@ -7,7 +7,6 @@
 window.addEventListener("click", goToMainMenuFromStartScreen);
 window.addEventListener("click", clearStartScreenInterval);
 
-// If the user clicks in the window, set the background color of <body> to yellow
 function goToMainMenuFromStartScreen() {
     window.removeEventListener("click", goToMainMenuFromStartScreen);
     goToNewScreen("mainMenu/mainMenu.html", "mainMenu/mainMenu.js");
@@ -45,40 +44,40 @@ function startAlphabethRain() {
     canvas.width = W;
     canvas.height = H;
 
-    //snowflake particles
-    var mp = 30; //max particles
+    //letters
+    var mp = 30; //max letters
     var particles = [];
     for (var i = 0; i < mp; i++)
     {
         particles.push({
             x: Math.random() * W, //x-coordinate
-            y: H, //y-coordinate
+            y: Math.random() * H, //y-coordinate
             r: Math.random() * 4 + 1, //radius
             d: Math.random() * mp, //density
             n: Math.round((Math.random() * 100) + 1),
-            s: Math.round((Math.random() * 100) + 1)
+            s: Math.round((Math.random() * 300) + 1),
+            cr: Math.round((Math.random() * 255) + 1),
+            cg: Math.round((Math.random() * 255) + 1),
+            cb: Math.round((Math.random() * 255) + 1),
         });
     }
 
-    //Lets draw the flakes
+    //Draw the letters
     function draw()
     {
         ctx.clearRect(0, 0, W, H);        
-        ctx.fillStyle = "rgba(0, 0, 0, 0.8)";
         ctx.beginPath();
         for (var i = 0; i < mp; i++)
         {
             var p = particles[i];
+            ctx.fillStyle = "rgba(" + p.cr + ", " + p.cg + ", " + p.cb +", 0.9)";
             ctx.font= p.s + "px Verdana";
             ctx.fillText(p.n, p.x, p.y);
-            //ctx.moveTo(p.x, p.y);
-            //ctx.arc(p.x, p.y, p.r, 0, Math.PI * 2, true);
         }
         update();
     }
 
-    //Function to move the snowflakes
-    //angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
+    //Function to move the letters
     var angle = 0;
     function update()
     {
@@ -86,31 +85,25 @@ function startAlphabethRain() {
         for (var i = 0; i < mp; i++)
         {
             var p = particles[i];
-            //Updating X and Y coordinates
-            //We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
-            //Every particle has its own density which can be used to make the downward movement different for each flake
-            //Lets make it more random by adding in the radius
             p.y += Math.cos(angle + p.d) + 1 + p.r / 2;
             p.x += Math.sin(angle) * 2;
-
-            //Sending flakes back from the top when it exits
-            //Lets make it a bit more organic and let flakes enter from the left and right also.
-            if (p.x > W + 5 || p.x < -5 || p.y > H)
+            
+            if (p.x > W + 15 || p.x < -15 || p.y > H + 200)
             {
-                if (i % 3 > 0) //66.67% of the flakes
+                if (i % 3 > 0) //66.67% of the letters
                 {
-                    particles[i] = {x: Math.random() * W, y: -10, r: p.r, d: p.d};
+                    particles[i] = {x: Math.random() * W, y: -10, r: p.r, d: p.d, n: p.n, s: p.s, cr: p.cr, cg: p.cg, cb: p.cb};
                 } else
                 {
-                    //If the flake is exitting from the right
+                    //If the letter is exitting from the right
                     if (Math.sin(angle) > 0)
                     {
                         //Enter from the left
-                        particles[i] = {x: -5, y: Math.random() * H, r: p.r, d: p.d};
+                        particles[i] = {x: -5, y: Math.random() * H, r: p.r, d: p.d, n: p.n, s: p.s, cr: p.cr, cg: p.cg, cb: p.cb};
                     } else
                     {
                         //Enter from the right
-                        particles[i] = {x: W + 5, y: Math.random() * H, r: p.r, d: p.d};
+                        particles[i] = {x: W + 5, y: Math.random() * H, r: p.r, d: p.d, n: p.n, s: p.s, cr: p.cr, cg: p.cg, cb: p.cb};
                     }
                 }
             }
